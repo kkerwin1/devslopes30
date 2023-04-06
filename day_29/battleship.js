7356dca6310274f9dc24499070e26fa8de8048cb
Kristopher H Kerwin <kkerwin1@gmail.com>
2023-04-06 14:57:59 -0500

    added day_29 comments/docstrings

--- day_29/battleship.js
+++ day_29/battleship.js
@@ -18,15 +18,25 @@ class Interface {
                               "C1", "C2", "C3"];
     }
 
+    /**
+     * Display a message. We wrap console.log() here to permit future API/interface changes (such as moving to a webpage).
+     * @param {String} message - Message to display.
+     */
     display(message) {
         console.log(message);
     }
 
+    /**
+     * Prompt player to press a key prior to playing the game.
+     */
     prompt_onLoad() {
         this.rs.keyInPause("Press any key to start the game. ");
         this.runMain();
     }
 
+    /**
+     * Prompt the player for a set of coordinates.
+     */
     prompt_coordinates() {
         const options = {
             limit: this.possibilities,
@@ -36,6 +46,9 @@ class Interface {
         this.board.processGuess(coordinateString);
     }
 
+    /**
+     * Upon completion of a single match, prompt the player if they want to play again.
+     */
     prompt_playAgain() {
         let response = this.rs.keyInYNStrict("You have destroyed all battleships. Would you like to play again? ");
         if (response) {
@@ -46,6 +59,9 @@ class Interface {
         }
     }
 
+    /**
+     * Run a single match.
+     */
     runMain() {
         while (this.board.shipsSunk < 2) {
             this.prompt_coordinates();
@@ -80,10 +96,15 @@ class Board {
     this.populateBoard();
     }
 
+    /**
+     * Process a guess to see if it is a hit or a miss.
+     * @param {String} string - String of format [Letter][Number], where [Letter] is A-C, and [Number] is 1-3.
+     */
     processGuess(string) {
         let x = string[1];
         let y = string[0];
 
+        // This is an empty square.
         if (this.grid[x][y] === 0) {
             this.grid[x][y] = 1;
             this.interface.display("You have missed!");
@@ -103,6 +124,9 @@ class Board {
         }                
     }
 
+    /**
+     * Populate the board with ships.
+     */
     populateBoard() {
         let shipsLeft = 2;
         while (shipsLeft > 0) {
@@ -110,14 +134,19 @@ class Board {
             let y = randomChoice(Object.keys(this.grid[x]));
 
             if (this.grid[x][y] === 0) {
+                // Square is empty, put a ship there.
                 this.grid[x][y] = false;
                 shipsLeft--;
             } else {
+                // Square already has a ship; find a different square.
                 continue;
             }
         }
     }
 
+    /**
+     * Reset the board for a new game.
+     */
     resetBoard() {
         this.grid = {
             1: {
@@ -141,6 +170,9 @@ class Board {
     }
 }
 
+/**
+ * Run the program.
+ */
 function main() {
     const board = new Board();
     board.interface.prompt_onLoad();
